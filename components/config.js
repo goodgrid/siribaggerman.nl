c79c3414e90704797ac92cad71f83034c3d0a1d7
Koen Bonnet <koen@goodgrid.nl>
2021-07-30 20:43:41 +0200

    Troubleshoot lack of config settings

--- components/config.js
+++ components/config.js
@@ -1,7 +1,9 @@
 
 export const Config = {
-    strapiHost: process.env.strapiHost, //"https://goodgrid-strapi.sloppy.zone",
-    matomoHost: process.env.matomoHost,//"https://goodgrid.nl/matomo/",
-    matomoSiteId: process.env.matomoSiteId //3
+    strapiHost: ((process.env.strapiHost!==undefined)?process.env.strapiHost:"https://goodgrid-strapi.sloppy.zone"),
+    matomoHost: ((process.env.matomoHost!==undefined)?process.env.matomoHost:"https://goodgrid.nl/matomo/"),
+    matomoSiteId: ((process.env.matomoSiteId!==undefined)?process.env.matomoSiteId:3)
 }
 
+
+console.log(Config);
